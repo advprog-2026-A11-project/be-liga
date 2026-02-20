0c6c14c54a888dd521108992b02757dbb22e7ab0
daffuwu <dananda41661@gmail.com>
2026-02-20 19:22:07 +0700

    Reorder and regroup static imports

--- src/test/java/id/ac/ui/cs/advprog/liga/repository/ClanRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/liga/repository/ClanRepositoryTest.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.liga.repository;
 import id.ac.ui.cs.advprog.liga.model.Clan;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClanRepositoryTest {
