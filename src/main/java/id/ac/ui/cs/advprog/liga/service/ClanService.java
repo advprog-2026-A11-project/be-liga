64db2eb76a293d3d8667c3bf35fbe32438a47121
daffuwu <dananda41661@gmail.com>
2026-02-20 14:37:57 +0700

    Add service

---
+++ src/main/java/id/ac/ui/cs/advprog/liga/service/ClanService.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.liga.service;
+import id.ac.ui.cs.advprog.liga.model.Clan;
+import java.util.List;
+
+public interface ClanService {
+    Clan create(Clan clan);
+    List<Clan> findAll();
+    Clan findById(String id);
+    void update(Clan clan);
+    void delete(String id);
+    // Member operations
+    void addMember(String clanId, int score);
+    void editMember(String clanId, int memberIndex, int newScore);
+    void deleteMember(String clanId, int memberIndex);
+}
