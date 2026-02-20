bcd62eab41969ce3c2e75824d2b84e4510bce76b
daffuwu <dananda41661@gmail.com>
2026-02-20 19:08:57 +0700

    Add unit tests for clan controller

---
+++ src/test/java/id/ac/ui/cs/advprog/liga/controller/ClanControllerTest.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.advprog.liga.controller;
+
+import id.ac.ui.cs.advprog.liga.service.ClanService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ClanController.class)
+class ClanControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private ClanService clanService;
+
+  @Test
+  void testListClansPage() throws Exception {
+    mockMvc.perform(get("/clan/list"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("ClanList"));
+  }
+
+  @Test
+  void testCreateClanPost() throws Exception {
+    mockMvc.perform(post("/clan/create")
+                    .param("clanName", "Test Clan"))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrl("list"));
+  }
+}
