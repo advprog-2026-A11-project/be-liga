21c0c11eedf6babf8efce80ba63d16cc997b9c1d
daffuwu <dananda41661@gmail.com>
2026-02-20 14:33:27 +0700

    Add model

---
+++ src/main/java/id/ac/ui/cs/advprog/liga/model/Clan.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.liga.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Getter @Setter
+public class Clan {
+    private String clanId;
+    private String clanName;
+    private List<Integer> memberScores;
+
+    public Clan() {
+        this.clanId = UUID.randomUUID().toString();
+        this.memberScores = new ArrayList<>();
+    }
+
+    // Calculated attribute: Sum of all member scores
+    public int getClanScore() {
+        return memberScores.stream().mapToInt(Integer::intValue).sum();
+    }
+}
