0d9ac8b35e7ece49f18a337f3445c974aae44564
daffuwu <dananda41661@gmail.com>
2026-04-16 00:18:46 +0700

    Add SecurityConfig

---
+++ src/main/java/id/ac/ui/cs/advprog/liga/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.liga.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/clan/list", "/api/clan/detail/**").permitAll() // Anyone can view clans
+                    .anyRequest().authenticated() // Must be logged in to create/join/leave clans
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
+    return http.build();
+  }
+}
