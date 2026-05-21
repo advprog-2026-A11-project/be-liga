0bfa623c2e6f204aaf0f9f4461a38e3596032d8a
daffuwu <dananda41661@gmail.com>
2026-05-21 21:41:46 +0700

    [REFACTOR] change score update request

--- src/main/java/id/ac/ui/cs/advprog/liga/dto/ScoreUpdateRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/liga/dto/ScoreUpdateRequest.java
@@ -12,5 +12,6 @@ import lombok.Setter;
 public class ScoreUpdateRequest {
     private String userId;
     private int score;
-    private double accuracy;
+    private boolean isAQuiz;   // true = quiz completion, false = mission completion
+    private double accuracy;   // only meaningful when isAQuiz = true, otherwise ignored
 }
