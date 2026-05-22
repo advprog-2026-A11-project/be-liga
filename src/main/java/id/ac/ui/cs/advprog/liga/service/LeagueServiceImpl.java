867086f0be6d61db0e3093ab5c48164f03d4e89a
daffuwu <dananda41661@gmail.com>
2026-05-22 22:09:17 +0700

    fix: CustomImportOrder

--- src/main/java/id/ac/ui/cs/advprog/liga/service/LeagueServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/liga/service/LeagueServiceImpl.java
@@ -15,7 +15,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
