f655209d5c433d9eac2cf32f986b9a1a93d51498
daffuwu <dananda41661@gmail.com>
2026-05-22 10:53:16 +0700

    fix: LineLength error for Super-linter again

--- src/main/java/id/ac/ui/cs/advprog/liga/service/LeagueServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/liga/service/LeagueServiceImpl.java
@@ -22,7 +22,13 @@ public class LeagueServiceImpl implements LeagueService {
 
   private static final int PROMOTION_SLOTS = 2;
   private static final int DEGRADATION_SLOTS = 2;
-  private static final List<String> TIER_ORDER = List.of("Bronze", "Silver", "Gold", "Platinum", "Diamond");
+  private static final List<String> TIER_ORDER = List.of(
+    "Bronze", 
+    "Silver",
+    "Gold", 
+    "Platinum", 
+    "Diamond"
+  );
 
   private final ClanRepository clanRepository;
   private final ClanMemberRepository clanMemberRepository;
