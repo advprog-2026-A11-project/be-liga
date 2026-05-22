814bfcaf5b3133ac7abf52ff951f865445e8769f
daffuwu <dananda41661@gmail.com>
2026-05-22 18:47:20 +0700

    fix: CustomImportOrder

--- src/main/java/id/ac/ui/cs/advprog/liga/service/ClanService.java
+++ src/main/java/id/ac/ui/cs/advprog/liga/service/ClanService.java
@@ -2,7 +2,6 @@ package id.ac.ui.cs.advprog.liga.service;
 
 import id.ac.ui.cs.advprog.liga.model.Clan;
 import id.ac.ui.cs.advprog.liga.model.ClanMember;
-
 import java.util.List;
 
 public interface ClanService {
