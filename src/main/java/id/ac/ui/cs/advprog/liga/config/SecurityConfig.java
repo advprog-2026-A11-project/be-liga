7d366924a9adb32b1207753cf6865e1f2d78282d
daffuwu <dananda41661@gmail.com>
2026-05-22 10:10:50 +0700

    fix: CustomImportOrder error for super linter again

--- src/main/java/id/ac/ui/cs/advprog/liga/config/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/liga/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.liga.config;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +10,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
